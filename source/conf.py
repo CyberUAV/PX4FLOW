d187b91a60072e72a87cbb61b0bb91b1e08ee41f
Cosmos <135506283+CyberUAV@users.noreply.github.com>
2023-06-12 17:02:15 +0800

    logo change

--- source/conf.py
+++ source/conf.py
@@ -30,20 +30,20 @@ html_logo = '_static/logo.svg'
 html_logo_width = "200px"
 html_logo_height="100px"
 
-#html_theme_options = {
-#     'logo_only': False,
-#     'display_version': True,
-#     'prev_next_buttons_location': 'bottom',
-#     'style_external_links': False,
-#     'vcs_pageview_mode': '',
-#     'style_nav_header_background': 'white',
-#     # Toc options
-#     'collapse_navigation': True,
-#     'sticky_navigation': True,
-#     'navigation_depth': 4,
-#     'includehidden': True,
-#     'titles_only': False
-# }
+html_theme_options = {
+    'logo_only': True,
+    # 'display_version': True,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
 # #html_theme_options = {
 #     'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
 #     'analytics_anonymize_ip': False,
